a2941ab5c1fed2aa5d563e0607b47a8431aa0d96
VegetableKitten <116822016+VegetableKitten@users.noreply.github.com>
2022-10-31 14:42:04 +0800

    Update CNN.py

--- Code/CNN.py
+++ Code/CNN.py
@@ -1,5 +1,6 @@
 from cProfile import label
 from operator import index
+from turtle import color
 import torch
 import torchvision
 from torch.utils.data import DataLoader
@@ -23,6 +24,24 @@ def one_hot (label):
     out.scatter_(dim=1,index=idx,value=1)
     return out
 
+def Visualization(data):
+    fig = plt.figure()
+    plt.plot(range(len(data)),data,color='blue')
+    plt.legend(['value'],loc='upper right')
+    plt.xlabel("step")
+    plt.ylabel("value")
+    plt.show()
+
+def plot_image(img,label,name):
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2,3,i+1)
+        plt.tight_layout()
+        plt.imshow(img[i][0]*0.3081+0.1037,cmap='gray',interpolation=None)
+        plt.title("{}:{}".format(name,label[i].item()))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
 
 train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST('./data/', train=True, download=True,
@@ -42,6 +61,7 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=batch_size_test, shuffle=True)
  
 class Net(nn.Module):
+
     def __init__(self):
         super(Net, self).__init__()
         self.fc1=nn.Linear(28*28,256)
@@ -55,16 +75,39 @@ class Net(nn.Module):
         return x
 
 net = Net()
+train_loss = []
 # [w1,b1,w2,b2,w3,b3]
 optimizer = optim.SGD(net.parameters(),lr=0.01,momentum=0.09)
-
 for epoch in range(3):
     for batch_idx,(x,y) in enumerate(train_loader):
         x = x.view(x.size(0), 28*28)
-        out = Net(x)
+        out = net(x)
         y_one_hot=one_hot(y)
         loss = F.mse_loss(out,y_one_hot)
+        train_loss.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         # w' = w-lr*grad
         optimizer.step()
+
+        if batch_idx % 10 == 0:
+            print(epoch, batch_idx , loss.item())
+
+# we get optimal [w1,b1,w2,b2,w3,b3]
+Visualization(train_loss)
+total_correct = 0
+for x,y in test_loader:
+    x = x.view(x.size(0),28*28)
+    out = net(x)
+    pred = out.argmax(dim=1)
+    correct = pred.eq(y).sum().float().item()
+    total_correct += correct
+total_num = len(test_loader.dataset)
+acc = total_correct/total_num
+print("test acc:",acc)
+
+# Check the effect
+x,y = next(iter(train_loader))
+out = net(x.view(x.size(0),28*28))
+pred = out.argmax(dim=1)
+plot_image(x,pred,'test')
